ad1e1a76d95b7b2d3dc20bcf9ccef095bd2b2cab
Akhilesh Soni <akhileshsoni95@gmail.com>
2020-04-29 16:25:33 -0500

    sddp updated demand model

--- mssp.py
+++ mssp.py
@@ -17,18 +17,26 @@ class mssp:
         self.distance=data.distance; #dictionary, eg D={('Wisconsin,Newyork'):2300}
         self.inventory=data.inventory;#initial inventory, eg I={'Wisconsin':24}
         self.penalty=data.penalty; #penalty per unit of unmet demand
-        self.demand=data.demand; #demand data for each stage, demand={('#stage(t),#node(scenario),state(name)'):450}
+        self.mean_demand=data.mean_demand; #demand data for each stage, demand={('#stage(t),#node(scenario),state(name)'):450}
         self.stages=data.stages; #list of integers
         self.scenarios=data.scenarios; #dict with # scenarios in each stage, scenarios={stage:[1,2,3..]}
+        self.xi=data.xi;
+        self.omega=data.omega;
         #models
         #self.fwd_model={}, # a model for each stage which keeps getting updated with new cuts
         #self.bwd_model={} #a model for each stage which gets updated with new cuts, diff scenarios solved by updating RHS
         #storing solutions
         self.prevStageVent={0:data.inventory}
+        self.prevStageFactor={0:data.initialFactor}
         for i in self.stages:
             self.prevStageVent[i]={};
+            self.prevStageFactor[i]={}
+        #cut coeffs
         self.alpha={};
+        #dual vars
         self.rho={};
+        self.gamma={};
+        #storing vals in sddp
         self.fwdObjVal={};
         self.costFwdPass={};
         self.paths={}; #storing paths generated randomly
@@ -55,11 +63,13 @@ class mssp:
         u=m.addVars(self.states,lb=0,name='vent-avail'); #stage
         y=m.addVars(self.states,lb=0,name='vent-unused'); #recourse
         z=m.addVars(self.states,lb=0,obj=self.penalty,name='vent-shortage'); #recourse
+        f=m.addVars(self.states,lb=0,name='demand-factor-var')  
         m.addVar(obj=1,lb=0,name='theta')
         #add constraints
         m.addConstrs((u[s]-x.sum('*',s)+x.sum(s,'*')==0 for s in self.states),name="flow-balance")
-        m.addConstrs((z[s]+u[s]-y[s]==0 for s in self.states),name="demand-balance")
+        m.addConstrs((z[s]+u[s]-y[s]-f[s]==0 for s in self.states),name="demand-balance")
         m.addConstrs((x.sum(s,'*')-y[s]<=0 for s in self.states),name="max-flow") 
+        m.addConstrs((f[s]==self.prevStageFactor[0][s] for s in self.states),name='demand-factor-con')
         m.setParam(GRB.Param.LogToConsole,0)
         m.update()
         return m
@@ -69,16 +79,22 @@ class mssp:
             self.fwd_model={};
             for t in self.stages:
                 self.fwd_model[t]=self.model(); #initialisae fwd model for each stage
+                for s in self.states:
+                    self.fwd_model[t].chgCoeff(self.fwd_model[t].getConstrByName("demand-balance[{}]".format(s)),self.fwd_model[t].getVarByName("demand-factor-var[{}]".format(s)),-self.mean_demand[(t,s)])
+                    self.fwd_model[t].update()
         #solve m in each stage using previous stage state variables and a sample path
         for t in self.stages:
             for s in self.states:
-                #update RHS
+                #update RHS and f coeffs
                 self.fwd_model[t].getConstrByName("flow-balance[{}]".format(s)).setAttr(GRB.Attr.RHS, self.prevStageVent[t-1][s])                
-                self.fwd_model[t].getConstrByName("demand-balance[{}]".format(s)).setAttr(GRB.Attr.RHS, self.demand[(t,path[t],s)])                
+                #self.fwd_model[t].getConstrByName("demand-balance[{}]".format(s)).setAttr(GRB.Attr.RHS, self.demand[(t,path[t],s)])
+                if t>1:
+                    self.fwd_model[t].getConstrByName("demand-factor-con[{}]".format(s)).setAttr(GRB.Attr.RHS, self.omega*self.prevStageFactor[t-1][s]+(1-self.omega)*self.xi[(t,path[t],s)])
             #add cuts, just add for current iteration, 
             if iterNum>1 and t!=self.stages[-1]:
                 self.fwd_model[t].addConstr((len(self.scenarios[t])*self.fwd_model[t].getVarByName('theta')>=sum(self.alpha[iterNum-1,j,t+1] for j in self.scenarios[t])+
-                sum(self.rho[iterNum-1,j,t+1,s]*self.fwd_model[t].getVarByName('vent-avail[{}]'.format(s)) for s in self.states for j in self.scenarios[t]))
+                sum(self.rho[iterNum-1,j,t+1,s]*self.fwd_model[t].getVarByName('vent-avail[{}]'.format(s)) for s in self.states for j in self.scenarios[t])+
+                self.omega*sum(self.gamma[iterNum-1,j,t+1,s]*self.fwd_model[t].getVarByName('demand-factor-var[{}]'.format(s)) for s in self.states for j in self.scenarios[t]))
                 ,name="cut-{}".format(iterNum))
             #update the model
             self.fwd_model[t].update()
@@ -93,31 +109,38 @@ class mssp:
             #store solution vals
             for s in self.states:                
                 self.prevStageVent[t][s]=self.fwd_model[t].getVarByName("vent-avail[{}]".format(s)).x
+                self.prevStageFactor[t][s]=self.fwd_model[t].getVarByName("demand-factor-var[{}]".format(s)).x
 
     def backward_pass(self,iterNum):
         if iterNum==1:
             self.bwd_model={}
             for t in self.stages:
                 self.bwd_model[t]=self.model();
+                for s in self.states:
+                    self.bwd_model[t].chgCoeff(self.bwd_model[t].getConstrByName("demand-balance[{}]".format(s)),self.bwd_model[t].getVarByName("demand-factor-var[{}]".format(s)),-self.mean_demand[(t,s)])
+                self.bwd_model[t].update()
+
         #solve m in each stage and for each scenario
         for t in self.stages:
             #in reverse order
             t=self.stages[-t];
-            #update RHS
+            #update RHS and coeff
             for s in self.states:
-                self.bwd_model[t].getConstrByName("flow-balance[{}]".format(s)).setAttr(GRB.Attr.RHS, self.prevStageVent[t-1][s])                
+                self.bwd_model[t].getConstrByName("flow-balance[{}]".format(s)).setAttr(GRB.Attr.RHS, self.prevStageVent[t-1][s])
                 self.bwd_model[t].update()
             #add cuts
             if t!=self.stages[-1]:
                 self.bwd_model[t].addConstr((len(self.scenarios[t])*self.bwd_model[t].getVarByName('theta')>=sum(self.alpha[iterNum,j,t+1] for j in self.scenarios[t])+
-                sum(self.rho[iterNum,j,t+1,s]*self.bwd_model[t].getVarByName('vent-avail[{}]'.format(s)) for s in self.states for j in self.scenarios[t]))
+                sum(self.rho[iterNum,j,t+1,s]*self.bwd_model[t].getVarByName('vent-avail[{}]'.format(s)) for s in self.states for j in self.scenarios[t])+
+                self.omega*sum(self.gamma[iterNum,j,t+1,s]*self.bwd_model[t].getVarByName('demand-factor-var[{}]'.format(s)) for s in self.states for j in self.scenarios[t]))
                 ,name="cut-{}".format(iterNum))
                 self.bwd_model[t].update()
             for j in self.scenarios[t]:
                 for s in self.states:
                     #update RHS
-                    self.bwd_model[t].getConstrByName("demand-balance[{}]".format(s)).setAttr(GRB.Attr.RHS, self.demand[(t,j,s)])               
-                #add cuts
+                    #self.bwd_model[t].getConstrByName("demand-balance[{}]".format(s)).setAttr(GRB.Attr.RHS, self.demand[(t,j,s)])               
+                    if t>1:
+                        self.bwd_model[t].getConstrByName("demand-factor-con[{}]".format(s)).setAttr(GRB.Attr.RHS, self.omega*self.prevStageFactor[t-1][s]+(1-self.omega)*self.xi[(t,j,s)])
                 #update the model
                 self.bwd_model[t].update()
                 #optimize
@@ -125,9 +148,10 @@ class mssp:
                 #store dual vals used for gen cuts
                 for s in self.states:
                     self.rho[iterNum,j,t,s]=self.bwd_model[t].getConstrByName("flow-balance[{}]".format(s)).pi
+                    self.gamma[iterNum,j,t,s]=self.bwd_model[t].getConstrByName("demand-factor-con[{}]".format(s)).pi
                 #store alpha vals
                 if t!=self.stages[0]:
-                    self.alpha[iterNum,j,t]=self.bwd_model[t].objVal-sum(self.rho[iterNum,j,t,s]*self.prevStageVent[t-1][s] for s in self.states)        
+                    self.alpha[iterNum,j,t]=self.bwd_model[t].objVal-sum(self.rho[iterNum,j,t,s]*self.prevStageVent[t-1][s] for s in self.states)-self.omega*sum(self.gamma[iterNum,j,t,s]*self.prevStageFactor[t-1][s] for s in self.states)        
     
     def execute_sddp(self,epsilon=0.01):
         convergence=False;
@@ -162,6 +186,7 @@ class mssp:
 
 
     def perfect_info(self,path):
+        #needs to be fixed
         piModel=Model()
         '''don't add cuts in this function, just build initial model
         time/scenario index not needed
@@ -210,7 +235,7 @@ class mssp:
         plt.xlabel('Iteration',fontsize=16)
         plt.ylabel('Objective Value',fontsize=16)
         plt.title('SDDP convergence',fontsize=16)
-        plt.savefig('sddp-convergence.png')
+        plt.savefig('sddp-convergence-new.png')
 
     def cal_EVPI(self):
         #call model and fix first stage decisions
@@ -253,6 +278,6 @@ if __name__=='__main__':
     data.gen_data()
     model=mssp(data,200);
     model.execute_sddp()
-    model.solvePI()
-    #model.plotBoundsSDDP()
-    model.cal_EVPI()
+    #model.solvePI()
+    model.plotBoundsSDDP()
+    #model.cal_EVPI()
